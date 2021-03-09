31287d390204f28545ba8bbdc3f8c46ae66e5442
kinfi4 <ilyamakarov223@gmail.com>
2021-03-09 18:05:57 +0200

    initial commit, api app created, virtual env configured

---
+++ backend/diary/api/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from datetime import datetime
+
+
+class DailyRecord(models.Model):
+    title = models.CharField(max_length=128, default=datetime.now().strftime('%Y%m%d %H:%M'))
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
