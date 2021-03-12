4acf28f8fa38527f83a9adf1e746ef627a491dc6
kinfi4 <ilyamakarov223@gmail.com>
2021-03-12 22:13:32 +0200

    author of the record field created

--- backend/diary/api/models.py
+++ backend/diary/api/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 from datetime import datetime
 
 
 class DailyRecord(models.Model):
     title = models.CharField(max_length=128, default=datetime.now().strftime('%Y/%m/%d %H:%M'))
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='records', null=True)
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
 
